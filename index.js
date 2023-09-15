102bbcde498ac71bc49dad6bbecbfa9ac14e7fcb
amarylliselpidia <elnina20001@gmail.com>
2023-09-15 21:36:29 +0700

    add info

---
+++ index.js
@@ -0,0 +1,21 @@
+var firstname ="Amaryllis"
+const age =19
+console.log("nama "+firstname)
+console.log("age "+age)
+
+if (firstname=="Amaryllis") {
+    console.log("benar")
+} else {
+    console.log("salah")
+}
+var jeniskelamin ="perempuan"
+switch (jeniskelamin) {
+    case "laki-laki":
+        console.log("laki-laki")
+        break;
+    case "perempuan":
+        console.log("perempuan")
+        break;
+    default:
+        break;
+}
