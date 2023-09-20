f20f2f40380425eb7feba9ef3079aba2a8c5fe07
amarylliselpidia <elnina20001@gmail.com>
2023-09-20 22:25:01 +0700

    Praktek perulangan dan fungsi

---
+++ perulangandanfungsi.js
@@ -0,0 +1,13 @@
+// Perulangan
+const numbers = [1, 2, 3, 4, 5];
+for (const num of numbers) {
+  console.log(num);
+}
+
+// Fungsi
+function multiply(a, b) {
+  return a * b;
+}
+
+const result = multiply(3, 3);
+console.log(result);
