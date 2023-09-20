2b80a3f99741e6ef0add44b9e357a5d2de633ca1
amarylliselpidia <elnina20001@gmail.com>
2023-09-20 22:24:20 +0700

    Praktek array dan object

---
+++ arraydanobject.js
@@ -0,0 +1,13 @@
+// Operasi pada Array
+const fruits = ['apple', 'banana', 'cherry'];
+fruits.push('orange');
+console.log(fruits);
+
+// Operasi pada Objek
+const person = {
+  name: 'Amaryllis',
+  age: 21,
+};
+
+person.gender = 'female';
+console.log(person);
